4d7be0b1e4e7d1edcd30824b5170efdb1b88dee1
Xiaobo Wang <yunruow@gmail.com>
2022-05-23 10:20:43 -0700

    update to drop 3.3

--- src/step-2-install-gpu-driver.sh
+++ src/step-2-install-gpu-driver.sh
@@ -10,7 +10,7 @@
 
 
 REQUIRED_KERNEL_VERSION="5.11.0-1028-azure"
-DRIVER_PACKAGE=APM_470.10.08_5.11.0-1028.31.tar
+DRIVER_PACKAGE=APM_470.10.10_5.11.0-1028.31.tar
 
 
 ## Install gpu driver required dependency and driver itself. It will reboot the system at the end.
