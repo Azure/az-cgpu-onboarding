9b290fd297ebb694f6d7ed810fb6e5778d0ac237
Xiaobo Wang <yunruow@gmail.com>
2022-05-25 12:04:46 -0700

    add verification steps

--- src/step-3-attestation.sh
+++ src/step-3-attestation.sh
@@ -9,17 +9,25 @@
 ##		bash step-2-install-gpu-driver.sh
 ##
 
+REQUIRED_DRIVER_INTERFACE_VERSION="NVIDIA System Management Interface -- v470.10.10"
 
 attestation(){
-	tar -xvf verifier_apm_pid3_3.tar
-	cd verifier_apm_pid3_3
-	sudo apt install python3-pip
-	sudo pip3 install -r requirements.txt
-	sudo pip3 install -e pynvml_src/
+	current_driver_interface_version=$(sudo nvidia-smi -h | head -1)
+    if [ "$current_driver_interface_version" != "$REQUIRED_DRIVER_INTERFACE_VERSION" ]; 
+    then
+    	echo "Current gpu driver version: ($current_driver_interface_version), Expected: ($REQUIRED_DRIVER_INTERFACE_VERSION)."
+    	echo "Please retry step-2-install-gpu-driver."
+    else 
+    	echo "Driver verified successfully, start attestation."
+		tar -xvf verifier_apm_pid3_3.tar
+		cd verifier_apm_pid3_3
+		sudo apt install python3-pip
+		sudo pip3 install -r requirements.txt
+		sudo pip3 install -e pynvml_src/
 
-	sudo python3 cc_admin.py
+		sudo python3 cc_admin.py
+	fi
 }
-
 
 
 if [[ "${#BASH_SOURCE[@]}" -eq 1 ]]; then
--- src/step-4-install-gpu-tools.sh
+++ src/step-4-install-gpu-tools.sh
@@ -11,43 +11,52 @@
 
 ## install dockder dependency.
 install_gpu_tools(){
-    # Install Docker
-    sudo apt-get update -y
 
-    sudo apt-get install \
-         ca-certificates \
-         curl \
-          gnupg \
-         lsb-release
+    attestation_result=$(bash step-3-attestation.sh | tail -1| sed -e 's/^[[:space:]]*//')
+    attestation_result=${attestation_result##*( )}
+    if [ "$attestation_result" != "GPU 0 verified successfully." ]; 
+    then
+      echo "Current gpu attestation failed: ${attestation_result}, expected: GPU 0 verified successfully."
+      echo "Please verify previous steps and retry attestation."
+    else 
+      echo "Attestation successfully, start docker installation."
+      # Install Docker
+      sudo apt-get update -y
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+      sudo apt-get install \
+           ca-certificates \
+           curl \
+            gnupg \
+           lsb-release
 
-    echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-    sudo apt-get update -y
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+      echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-    sudo docker run hello-world
+      sudo apt-get update -y
+      sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-    # Nvidia Container Toolkit
-    distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-          && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-          && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-                sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-                sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+      sudo docker run hello-world
 
-    curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
-            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+      # Nvidia Container Toolkit
+      distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+            && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+            && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+                  sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+                  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-    sudo apt-get update
-    sudo apt-get install -y nvidia-docker2
-    sudo systemctl restart docker
+      curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+              sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+              sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-    sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+      sudo apt-get update
+      sudo apt-get install -y nvidia-docker2
+      sudo systemctl restart docker
 
+      sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
+    fi
 }
 
 if [[ "${#BASH_SOURCE[@]}" -eq 1 ]]; then
