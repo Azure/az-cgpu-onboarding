b342dcfdfead4213ab4eaf24d64a8f23939c72ba
Anjuli Goring <anjuligoring@gmail.com>
2022-06-30 10:31:45 -0700

    Update step-2-attestation.sh
    update expected driver & pid versions

--- src/step-2-attestation.sh
+++ src/step-2-attestation.sh
@@ -9,7 +9,7 @@
 ##		bash step-2-attestation.sh
 ## 
 
-REQUIRED_DRIVER_INTERFACE_VERSION="NVIDIA System Management Interface -- v470.10.10"
+REQUIRED_DRIVER_INTERFACE_VERSION="NVIDIA System Management Interface -- v470.10.11"
 MAX_RETRY=3
 
 attestation(){
@@ -21,8 +21,8 @@ attestation(){
     	echo "Please retry step-1-install-gpu-driver."
     else 
     	echo "Driver verified successfully, start attestation."
-		tar -xvf verifier_apm_pid3_3.tar
-		cd verifier_apm_pid3_3
+		tar -xvf verifier_apm_pid3_4.tar
+		cd verifier_apm_pid3_4
 		sudo apt install python3-pip
 		sudo pip3 install -r requirements.txt
 		sudo pip3 install -e pynvml_src/
