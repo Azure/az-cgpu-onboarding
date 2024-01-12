54d26ee21e5312996ba82ba74993dacead181a59
zhuke123 <zkwestlife@gmail.com>
2024-01-11 18:06:52 -0800

    Fix linux paramters

--- src/cmk_module/Linux/cgpu-deploy-cmk-des.sh
+++ src/cmk_module/Linux/cgpu-deploy-cmk-des.sh
@@ -1,124 +1,117 @@
 #!/bin/bash
-  ###Sample Parameters:
-  #timeString=$(date +"%H%M%S")
-  #subscriptionId = "85c61f94-8912-4e82-900e-6ab44de9bdf8"
-  #region = "eastus2"
-  #resourceGroup ="$timeString-CMK-rg"
-  #keyName = "$timeString-CMK-key"
-  #keyVault = "$timeString-CMK-kv"
-  #policyPath = "skr-policy-2.json"
-  #desName = "$timeString-CMK-des"
-  #deployName = "$timeString-CMK-desdeploy"
-  #desArmTemplate = "deployDES.json"
-#
-  ###Sample Command:
-  #bash Linux/cgpu-deploy-cmk-des.sh \
-  #--subscriptionId $subscriptionId \
-  #--region $region \
-  #--resourceGroup $resourceGroup \
-  #--keyName $keyName \
-  #--keyVault $keyVault \
-  #--policyPath $policyPath \
-  #--desName $desName \
-  #--deployName $deployName \
-  #--desArmTemplate $desArmTemplate
 
-cvmAgentId="bf7b6499-ff71-4aa2-97a4-f372087be7f0"
-keySize=3072
-$region="eastus2"
+## s<TODO >You will need this step if you have not set your cvmAgentId for your tenant
+## Sample Command:
+## bash Linux/cgpu-deploy-cmk-des.sh \
+## -s "85c61f94-8912-4e82-900e-6ab44de9bdf8" \
+## -r "eastus2" \
+## -g "cmk-$(date +"%H%M%S")-rg" \
+## -k "cmk-$(date +"%H%M%S")-key" \
+## -v "cmk-$(date +"%H%M%S")-kv" \
+## -p "skr-policy.json" \
+## -d "cmk-$(date +"%H%M%S")-desdeploy" \
+## -n "cmk-$(date +"%H%M%S")-des" \
+## -t "deployDES.json"
 
-while getopts ":-" opt; do
-   case "$opt" in
-      -)
-         case "${OPTARG}" in
-            subscriptionId)
-               subscriptionId="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            region)
-               region="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            resourceGroup)
-               resourceGroup="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            keyVault)
-               keyVault="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            cvmAgentId)
-               cvmAgentId="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            keyName)
-               keyName="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            keySize)
-               keySize="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            policyPath)
-               policyPath="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            desName)
-               desName="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            deployName)
-               deployName="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            desArmTemplate)
-               desArmTemplate="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-               ;;
-            *)
-               echo "Unknown option --${OPTARG}"
-               exit 1
-               ;;
-         esac;;
-      \?)
-         echo "Invalid option: -${OPTARG}"
-         exit 1
+# Deploy CMK DES
+DEPLOY-CMK-DES() {
+
+   # Initialize variables
+   local subscriptionId=""
+   local region="eastus2"
+   local resourceGroup=""
+   local keyName=""
+   local keyVault=""
+   local policyPath=""
+   local desName=""
+   local deployName=""
+   local desArmTemplate=""
+   local keySize=3072
+   local cvmAgentId="bf7b6499-ff71-4aa2-97a4-f372087be7f0"
+
+   # Parse options
+   while getopts ":s:r:g:k:v:p:d:n:t:" opt; do
+      case $opt in
+         s) subscriptionId=$OPTARG;;
+         r) region=$OPTARG;;
+         g) resourceGroup=$OPTARG;;
+         k) keyName=$OPTARG;;
+         v) keyVault=$OPTARG;;
+         p) policyPath=$OPTARG;;
+         d) deployName=$OPTARG;;
+         n) desName=$OPTARG;;
+         t) desArmTemplate=$OPTARG;;
+         \?) echo "Invalid option -$OPTARG" >&2
+               return 1
          ;;
-   esac
-done
+      esac
+   done
 
+   echo "Parameters:"
+   echo "Subscription ID: $subscriptionId"
+   echo "Region: $region"
+   echo "Resource Group: $resourceGroup"
+   echo "Key Name: $keyName"
+   echo "Key Vault: $keyVault"
+   echo "Policy Path: $policyPath"
+   echo "DES Name: $desName"
+   echo "Deploy Name: $deployName"
+   echo "DES ARM Template: $desArmTemplate"
+   az login
+   az account set --subscription $subscriptionId
+   echo "---------------------------------- Login to [$subscriptionId] ----------------------------------"
 
-az login
-az account set --subscription $subscriptionId
-echo "---------------------------------- Login to [$subscriptionId] ----------------------------------"
+   groupExists=$(az group exists --name $resourceGroup)
+   groupExists=$(echo "$groupExists" | tr -cd '[:alnum:]')
+   if [ "$groupExists" == "false" ]; then
+      # Create the resource group since it does not exist
+      az group create --name $resourceGroup --location $region
+      echo "---------------------------------- Resource group [$resourceGroup] created ----------------------------------"
+   else
+      echo "---------------------------------- Resource group [$resourceGroup] already exists ----------------------------------"
+   fi
 
-az group create --name $resourceGroup --location $region
-az keyvault create --name $keyVault --resource-group $resourceGroup --location $region --sku Premium --enable-purge-protection
-echo "---------------------------------- KeyVault [$keyVault] created ----------------------------------"
+   az keyvault create --name $keyVault --resource-group $resourceGroup --location $region --sku Premium --enable-purge-protection
+   echo "---------------------------------- KeyVault [$keyVault] created ----------------------------------"
 
-cvmAgent=$(az ad sp show --id $cvmAgentId | jq -r '.id')
-az keyvault set-policy --name $keyVault --resource-group $resourceGroup --object-id $cvmAgent --key-permissions get release
-az keyvault key create --vault-name $keyVault --name $keyName --ops wrapKey unwrapkey --kty RSA-HSM --size $keySize --exportable true --policy $policyPath
-echo "---------------------------------- KeyName [$keyName] created ----------------------------------"
+   cvmAgent=$(az ad sp show --id $cvmAgentId | jq -r '.id')
+   az keyvault set-policy --name $keyVault --resource-group $resourceGroup --object-id $cvmAgent --key-permissions get release
+   az keyvault key create --vault-name $keyVault --name $keyName --ops wrapKey unwrapkey --kty RSA-HSM --size $keySize --exportable true --policy $policyPath
+   echo "---------------------------------- KeyName [$keyName] created ----------------------------------"
 
-encryptionKeyVaultId=$(az keyvault show -n $keyVault -g $resourceGroup --query "id" -o tsv)
-encryptionKeyVaultId=$(echo "$encryptionKeyVaultId" | tr -cd '[:alnum:]-/,.:')
-echo "---------------------------------- encryptionKeyVaultId [$encryptionKeyVaultId]----------------------------------"
+   encryptionKeyVaultId=$(az keyvault show -n $keyVault -g $resourceGroup --query "id" -o tsv)
+   encryptionKeyVaultId=$(echo "$encryptionKeyVaultId" | tr -cd '[:alnum:]-/,.:')
+   echo "---------------------------------- encryptionKeyVaultId [$encryptionKeyVaultId]----------------------------------"
 
-encryptionKeyURL=$(az keyvault key show --vault-name $keyVault --name $keyName --query "key.kid" -o tsv)
-encryptionKeyURL=$(echo "$encryptionKeyURL" | tr -cd '[:alnum:]-/,.:')  
-echo "---------------------------------- encryptionKeyURL [$encryptionKeyURL]----------------------------------"
+   encryptionKeyURL=$(az keyvault key show --vault-name $keyVault --name $keyName --query "key.kid" -o tsv)
+   encryptionKeyURL=$(echo "$encryptionKeyURL" | tr -cd '[:alnum:]-/,.:')  
+   echo "---------------------------------- encryptionKeyURL [$encryptionKeyURL]----------------------------------"
 
-az deployment group create \
-    -g $resourceGroup \
-    -n $deployName \
-    -f $desArmTemplate \
-    -p desName=$desName \
-    -p encryptionKeyURL=$encryptionKeyURL \
-    -p encryptionKeyVaultId=$encryptionKeyVaultId \
-    -p region=$region
-echo "---------------------------------- Des deployment [$desName] created----------------------------------"
+   az deployment group create \
+      -g $resourceGroup \
+      -n $deployName \
+      -f $desArmTemplate \
+      -p desName=$desName \
+      -p encryptionKeyURL=$encryptionKeyURL \
+      -p encryptionKeyVaultId=$encryptionKeyVaultId \
+      -p region=$region
+   echo "---------------------------------- Des deployment [$desName] created----------------------------------"
 
-desIdentity=$(az disk-encryption-set show -n $desName -g $resourceGroup --query "identity.principalId" -o tsv)
-desIdentity=$(echo "$desIdentity" | tr -cd '[:alnum:]-/,.:')
-echo "---------------------------------- desIdentity [$desIdentity] retrieved ----------------------------------"
+   desIdentity=$(az disk-encryption-set show -n $desName -g $resourceGroup --query "identity.principalId" -o tsv)
+   desIdentity=$(echo "$desIdentity" | tr -cd '[:alnum:]-/,.:')
+   echo "---------------------------------- desIdentity [$desIdentity] retrieved ----------------------------------"
 
-az keyvault set-policy -n $keyVault \
-    -g $resourceGroup \
-    --object-id $desIdentity \
-    --key-permissions wrapkey unwrapkey get
-echo "---------------------------------- Set policy ----------------------------------"
+   az keyvault set-policy -n $keyVault \
+      -g $resourceGroup \
+      --object-id $desIdentity \
+      --key-permissions wrapkey unwrapkey get
+   echo "---------------------------------- Set policy ----------------------------------"
 
-desID=$(az disk-encryption-set show -n $desName -g $resourceGroup --query [id] -o tsv)
-desID=$(echo "$desID" | tr -cd '[:alnum:]-/,.:')
-echo "---------------------------------- [$desID] is the desID ----------------------------------"
+   desID=$(az disk-encryption-set show -n $desName -g $resourceGroup --query [id] -o tsv)
+   desID=$(echo "$desID" | tr -cd '[:alnum:]-/,.:')
+   echo "---------------------------------- [$desID] is the desID ----------------------------------"
 
+}
+
+# call DEPLOY-CMK-DES and pass all the arguments
+DEPLOY-CMK-DES "$@" 2>&1 
--- src/cmk_module/README.md
+++ src/cmk_module/README.md
@@ -5,32 +5,43 @@
 
 ## On Linux Bash shell
 
-- Define Parameters
+- (Prerequisite) Set MgServicePrincipal
+<TODO >You will need this step if you have not set your cvmAgentId for your tenant
 ```
-resourceGroup="CMK-rg"
-keyVault="CMK-kv"
-policyPath="skr-policy.json"
-desName="CMK-des"
-deployName="CMK-desdeploy"
-desArmTemplate="deployDES.json"
-subscriptionId="<Your Subcription ID>"
-region="<Your Region>"
+az login;
+tenantId=$(az account show --query tenantId -o tsv)
+tenantId=$(echo "$tenantId" | tr -cd '[:alnum:]-/,.:')
+az ad sp create-for-rbac --name "Confidential VM Orchestrator" --skip-assignment
+az role assignment create --assignee "bf7b6499-ff71-4aa2-97a4-f372087be7f0" --role "Contributor" --scope /subscriptions/"85c61f94-8912-4e82-900e-6ab44de9bdf8"
 ```
+This has not been tested yet
 
-- Call the script with the parameters
+
+- Call bash script
 ```
-bash .\Linux\cgpu-deploy-cmk-des.sh \ 
-  -subscriptionId $subscriptionId \
-  -region $region \
-  -resourceGroup $resourceGroup \
-  -keyVault $keyVault \
-  -policyPath $policyPath \
-  -desName $desName `
-  -deployName $deployName `
-  -desArmTemplate $desArmTemplate
+bash Linux/cgpu-deploy-cmk-des.sh \
+-s "85c61f94-8912-4e82-900e-6ab44de9bdf8" \
+-r "eastus2" \
+-g "cmk-$(date +"%H%M%S")-rg" \
+-k "cmk-$(date +"%H%M%S")-key" \
+-v "cmk-$(date +"%H%M%S")-kv" \
+-p "skr-policy.json" \
+-d "cmk-$(date +"%H%M%S")-desdeploy" \
+-n "cmk-$(date +"%H%M%S")-des" \
+-t "deployDES.json"
 ```
 
 ## On Windows Powershell
+
+- (Prerequisite) Set MgServicePrincipal
+You will need this step if you have not set your cvmAgentId for your tenant
+```
+az login
+$tenatId= $(az account show --query tenantId -o tsv)
+Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery
+Connect-Graph -Tenant $tenatId -Scopes Application.ReadWrite.All
+New-MgServicePrincipal -AppId bf7b6499-ff71-4aa2-97a4-f372087be7f0 -DisplayName "Confidential VM Orchestrator"
+```   
 
 - Import Module
 ```
@@ -43,12 +54,12 @@ Import-Module -Name .\Windows\cgpu-deploy-cmk-des.psm1 -Force -DisableNameChecki
   $timeString = Get-Date -Format "HHmmss"
   $subscriptionId = "85c61f94-8912-4e82-900e-6ab44de9bdf8"
   $region = "eastus2"
-  $resourceGroup ="$($timeString)-CMK-rg"
-  $keyName = "$($timeString)-CMK-key"
-  $keyVault = "$($timeString)-CMK-kv"
-  $policyPath = "skr-policy-2.json"
-  $desName = "$($timeString)-CMK-des"
-  $deployName = "$($timeString)-CMK-desdeploy"
+  $resourceGroup ="CMK-$($timeString)-rg"
+  $keyName = "CMK-$($timeString)-key"
+  $keyVault = "CMK-$($timeString)-kv"
+  $policyPath = "skr-policy.json"
+  $desName = "CMK-$($timeString)-des"
+  $deployName = "CMK-$($timeString)-desdeploy"
   $desArmTemplate = "deployDES.json"
 ```
 
