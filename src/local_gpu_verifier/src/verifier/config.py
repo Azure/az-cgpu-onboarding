dbd67285313a858460ea6811fef5756943060713
Yu Fu <yfu@microsoft.com>
2024-04-01 16:14:26 -0700

    regression test

--- src/local_gpu_verifier/src/verifier/config.py
+++ src/local_gpu_verifier/src/verifier/config.py
@@ -59,7 +59,7 @@ class BaseSettings:
     AZURE_VM_REGION = ""
     AZURE_IMDS_URL = "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
     AZURE_THIM_ENDPOINT_DICT = {
-        "eastus2": 'https://useast2.thim.azure.net',
+        "eastus2": 'https://useast2.thim.azue.net',
         "centraluseuap": 'https://uscentraleuap.thim.azure.net',
         "lab": ''
     }
