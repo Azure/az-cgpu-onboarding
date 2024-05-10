20ddaefa209f97b0a3d4572d9b2d6ee354426d5f
anjuligoring <anjuligoring@gmail.com>
2024-05-10 11:58:13 -0700

    updating all links to point to release-3.0.4

--- src/cgpu-h100-auto-onboarding.sh
+++ src/cgpu-h100-auto-onboarding.sh
@@ -46,7 +46,7 @@ cgpu_h100_onboarding() {
 	    esac
 	done
 	
-	ONBOARDING_PACKAGE_VERSION="v3.0.3"
+	ONBOARDING_PACKAGE_VERSION="v3.0.4"
 	echo "Confidential GPU H100 Onboarding Package Version: $ONBOARDING_PACKAGE_VERSION"
 
 	if [ "$(az --version | grep azure-cli)" == "" ]; then
