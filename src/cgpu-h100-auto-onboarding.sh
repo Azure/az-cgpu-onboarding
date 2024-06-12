ac8ab65f3bb18db502d9e42ac20c23d5bc668305
Anjuli Goring <angoring@microsoft.com>
2024-06-12 15:48:21 -0700

    adding bash

--- src/cgpu-h100-auto-onboarding.sh
+++ src/cgpu-h100-auto-onboarding.sh
@@ -54,7 +54,7 @@ cgpu_h100_onboarding() {
 	    esac
 	done
 	
-	ONBOARDING_PACKAGE_VERSION="v3.0.4"
+	ONBOARDING_PACKAGE_VERSION="v3.0.5"
 	echo "Confidential GPU H100 Onboarding Package Version: $ONBOARDING_PACKAGE_VERSION"
 
 	if [ "$(az --version | grep azure-cli)" == "" ]; then
