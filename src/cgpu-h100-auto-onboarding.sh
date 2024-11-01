31948a18ee93797a87cbc86701246ecfc593d255
Jeremy Weiss <42979139+jeremy-weiss@users.noreply.github.com>
2024-11-01 14:17:16 -0700

    Revert "Merge release 3.1.0"

--- src/cgpu-h100-auto-onboarding.sh
+++ src/cgpu-h100-auto-onboarding.sh
@@ -59,7 +59,7 @@ cgpu_h100_onboarding() {
 	    esac
 	done
 	
-	ONBOARDING_PACKAGE_VERSION="V3.1.0"
+	ONBOARDING_PACKAGE_VERSION="V3.0.10"
 	echo "Confidential GPU H100 Onboarding Package Version: $ONBOARDING_PACKAGE_VERSION"
 
 	if [ "$(az --version | grep azure-cli)" == "" ]; then
